46e1fc41d86602c4eb74f536e22cdbf3150cd790
KoderMx1 <116667571+KoderMx1@users.noreply.github.com>
2023-01-25 14:40:45 -0800

    Update eyeTracking.py

--- eyeTracking.py
+++ eyeTracking.py
@@ -9,6 +9,40 @@ face_cascade = cv2.CascadeClassifier('face.xml')
 # Start the video stream
 capture = cv2.VideoCapture(0)
 
+def detect_pupil(img):
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray_image,127,255,0)
+    M = cv2.moments(thresh)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    centroid = (cX,cY)
+    dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+    blur = cv2.GaussianBlur(dst,(5,5),0)
+    inv = cv2.bitwise_not(blur)
+    thresh = cv2.cvtColor(inv, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((2,2),np.uint8)
+    erosion = cv2.erode(thresh,kernel,iterations = 1)
+    ret,thresh1 = cv2.threshold(erosion,210,255,cv2.THRESH_BINARY)
+    cnts, hierarchy = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    flag = 10000
+    final_cnt = None
+    for cnt in cnts:
+        (x,y),radius = cv2.minEnclosingCircle(cnt)
+        distance = abs(centroid[0]-x)+abs(centroid[1]-y)
+        if distance < flag :
+            flag = distance
+            final_cnt = cnt
+        else:
+            continue
+    (x,y),radius = cv2.minEnclosingCircle(final_cnt)
+    center = (int(x),int(y))
+    radius = int(radius)
+    cv2.circle(img,center,radius,(255,0,0),2)
+
+    text(str(center[0]) + " " + str(center[1]) + " " + str(radius), (10, 600), 1, (255, 255, 255))
+
+    return img
+
 def detect_blue_pixels(image, circleFrame):
     # Convert the image to the HSV color space
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -31,6 +65,27 @@ def detect_blue_pixels(image, circleFrame):
         cv2.circle(circleFrame, center_of_mass, int(np.sqrt(moments["m00"]/np.pi)), (0, 255, 0), 2)
     else:
         text('failed to find pupils', (10, 500), 1, (255, 255, 255))
+
+def detect_pupils(img):
+    # Load image
+    #img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Apply a Gaussian blur to the image to reduce noise
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    
+    # Use the Hough Circle Transform to detect circles in the image
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+    
+    # check if any pupils are detected
+    if circles is not None:
+        pupils = []
+        for circle in circles[0, :]:
+            x, y, r = circle
+            pupils.append((x, y, r))
+        return pupils
+    else:
+        return None
 
 def find_pupil(img, draw):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -129,11 +184,22 @@ while True:
 
         #detect_blue_pixels(eyeFrame, frame)
 
+        #leftPupil = detect_pupils(leftEyeImage)
+        #rightPupil = detect_pupils(rightEyeImage)
+
+        #text(leftPupil, (10, 500), 1, (255, 255, 255))
+        #text(leftPupil, (10, 600), 1, (255, 255, 255))
+
+        leftPupil = detect_pupil(leftEyeImage)
+        rightPupil = detect_pupil(rightEyeImage)
+
     # Display the resulting frame
     cv2.imshow('Pupil Tracking', frame)
     #cv2.imshow('Eye Detection', eyeFrame)
-    cv2.imshow('Right Eye', rightEyeImage)
-    cv2.imshow('Left Eye', leftEyeImage)
+    #cv2.imshow('Right Eye', rightEyeImage)
+    #cv2.imshow('Left Eye', leftEyeImage)
+    cv2.imshow('Right Eye', rightPupil)
+    cv2.imshow('Left Eye', leftPupil)
 
     # Stop the program if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
